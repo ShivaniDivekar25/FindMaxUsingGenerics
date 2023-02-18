89fa0186ff45f2f7916b19a009e532a1c5e67271
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-18 21:34:52 +0530

    Modified to Find Maximum Integer Number program

---
+++ FindMaxUsingGenerics/FindMaximumNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    public class FindMaximumNumber
+    {
+        public static void FindMaxInteger()
+        {
+            Console.WriteLine("Enter first number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+            if(firstNumber>secondNumber && firstNumber>thirdNumber)
+                Console.WriteLine("The {0} is greatest of all number",firstNumber);
+            else if(secondNumber>thirdNumber)
+                Console.WriteLine("The {0} is greatest of all number",secondNumber);
+            else
+                Console.WriteLine("The {0} is greatest of all number",thirdNumber);
+        }
+    }
+}
--- FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to find maximum using Generics");
+            Console.WriteLine("Enter an option");
+            Console.WriteLine("1:Find integer maximum number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FindMaximumNumber.FindMaxInteger();
+                    break;
+                default:
+                    Console.WriteLine("Enter correct option");
+                    break;
+            }
         }
     }
 }
