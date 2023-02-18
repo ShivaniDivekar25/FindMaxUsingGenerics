20a1e65123ef182d736c801fb599e493d849e731
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-18 21:52:10 +0530

    Modified to Find Float Number program

--- FindMaxUsingGenerics/FindMaximumNumber.cs
+++ FindMaxUsingGenerics/FindMaximumNumber.cs
@@ -24,5 +24,21 @@ namespace FindMaxUsingGenerics
             else
                 Console.WriteLine("The {0} is greatest of all number",thirdNumber);
         }
+        public static void FindMaxFloat()
+        {
+            Console.WriteLine("Enter first float number");
+            float firstNumber = (float)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second float number");
+            float secondNumber = (float)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter third float number");
+            float thirdNumber = (float)Convert.ToDouble(Console.ReadLine());
+
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0) 
+                Console.WriteLine("The {0} is greatest of all number", firstNumber);
+            else if (secondNumber.CompareTo(thirdNumber) > 0)
+                Console.WriteLine("The {0} is greatest of all number", secondNumber);
+            else
+                Console.WriteLine("The {0} is greatest of all number", thirdNumber);
+        }
     }
 }
--- FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/Program.cs
@@ -6,12 +6,15 @@
         {
             Console.WriteLine("Welcome to find maximum using Generics");
             Console.WriteLine("Enter an option");
-            Console.WriteLine("1:Find integer maximum number");
+            Console.WriteLine("1:Find integer maximum number\n2:Find float maximum number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     FindMaximumNumber.FindMaxInteger();
+                    break;
+                case 2:
+                    FindMaximumNumber.FindMaxFloat();
                     break;
                 default:
                     Console.WriteLine("Enter correct option");
